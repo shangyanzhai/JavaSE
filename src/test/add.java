4c02e227fca996fec94e8732c48b430d2dbf765c
季翔 <2091163852@qq.com>
2022-08-23 15:38:43 +0800

    add java 8_14 初学

--- src/test/add.java
+++ src/test/add.java
@@ -1,3 +1,5 @@
+package test;
+
 public class add {
     public static void main(String[] args) {
         int b = 0;
--- src/test/nian.java
+++ src/test/nian.java
@@ -1,3 +1,5 @@
+package test;
+
 public class nian {
     public static void main(String[] args) {
         int a = 1000;
