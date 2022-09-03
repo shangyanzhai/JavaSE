0d96b20b5ec76094922bfd9dc7d2d8610c52bc30
季翔 <2091163852@qq.com>
2022-09-03 22:01:39 +0800

    add java 8_25 初学

--- src/test/PrintfPrime.java
+++ src/test/PrintfPrime.java
@@ -2,15 +2,16 @@ package test;
 
 public class PrintfPrime {
     public static void main(String[] args) {
-        for(int i = 1;i < 101;i++){
-            if(i == 1||i == 2){
+        for(int i = 2;i < 101;i++){
+            if(i == 2){
                 System.out.println(i);
                 continue;
             }
             for(int j = 2;j < i ;j++) {
                 if (i % j == 0) {
                     break;
-                }else if(j + 1 == i){
+                }
+                if(j + 1 == i){
                     System.out.println(i);
                 }
             }
