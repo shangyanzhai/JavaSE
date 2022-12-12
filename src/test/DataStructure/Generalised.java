d349a855a60b8f6a1e3610d7fd8c65eb5481f712
季翔 <2091163852@qq.com>
2022-12-12 21:18:47 +0800

    add java 12_12  使用泛型 包装类 自动装拆箱

--- src/test/DataStructure/Generalised.java
+++ src/test/DataStructure/Generalised.java
@@ -11,13 +11,15 @@ import java.util.Scanner;
 public class Generalised {
     public static void main(String[] args) {
         /**
-         * <...>闭合的尖括号 ———— Java语法中泛型付润特征
+         * 泛型 -- 变化的类型
+         * <...>闭合的尖括号 ———— Java语法中泛型的特征
          * 泛型分为 定义泛型（类 or 方法）和 使用泛型
          * 现阶段，学会如何使用别人定义好的泛型类（Iterable ,Collection ,List）
          *
          * 泛型 Object[]
          * Object是所有类的祖先类
          * 上层引用可以指向下层对象
+         * 只能用于引用类型
          * Java 的泛型只在编译阶段起作用
          */
         Iterable<String> iterable = null;//现在的元素是String类型
@@ -33,17 +35,5 @@ public class Generalised {
 //        list2.add(1);//此时，除了字符串类型以外，放入其他类型都会报错
 //        list2.add("hello");
 //        list2.add(new Scanner(System.in));//此时，除了字符串类型以外，放入其他类型都会报错
-        /**
-         * Java 的泛型只支持引用，不支持基本类型
-         * 包装类：
-         * byte -》 class Byte
-         * short -》 class Short
-         * int -》 class Integer
-         * long -》 class Long
-         * float -》 class Float
-         * double -》 class Double
-         * char -》 class Charater
-         * boolean -》 class Boolean
-         */
     }
 }
